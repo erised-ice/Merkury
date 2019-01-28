58c85ef8711da510a77e47c5176ce23f548ae7ea
alexandra <le.onze.mai@gmail.com>
2019-01-28 23:46:14 +0300

    [alert]added alert

--- js/scripts.js
+++ js/scripts.js
@@ -3,48 +3,51 @@ $(document).ready(function() {
     dots: true
   });
 
-  function popup() {
-    var openPopupButtonCollection = $('.js-open-popup');
-    var closePopupCollection = $('.js-close-popup');
-    var popup = $('.js-popup');
-    var openPopup = function() {
-      popup.addClass('popup--show');
-    };
-    var closePopup = function() {
-      popup.removeClass('popup--show');
-    };
+  var openPopupButtonCollection = $('.js-open-popup');
+  var closePopupCollection = $('.js-close-popup');
+  var popup = $('.js-popup');
+  var openPopup = function() {
+    popup.addClass('popup--show');
+  };
+  var closePopup = function() {
+    popup.removeClass('popup--show');
+  };
 
-    openPopupButtonCollection.on('click', openPopup);
-    closePopupCollection.on('click', closePopup);
+  openPopupButtonCollection.on('click', openPopup);
+  closePopupCollection.on('click', closePopup);
 
-    $(document).on('keydown', function (event) {
-      if (event.keyCode == 27) {
-        closePopup()
-      }
-    });
-  }
-
-  function displayAlertSuccessSubmit() {
-    var href = window.location.href;
-    var hasSubmitHref = href.includes('submit');
-
-    if (hasSubmitHref) {
-      var alert = $('.js-alert');
-      var toggleVisibleAlert = function(isVisible) {
-        alert.toggle(isVisible);
-      };
-      var timeDisplayAlert = 3000;
-
-      toggleVisibleAlert(true);
-
-      setTimeout(function() {
-        toggleVisibleAlert(false);
-      }, timeDisplayAlert);
+  $(document).on('keydown', function (event) {
+    if (event.keyCode == 27) {
+      closePopup()
     }
+  });
+
+  function pressButton() {
+    var form = $('.js-popup-form');
+    var submitedForm = function(event) {
+      event.preventDefault();
+      
+      closePopup();
+      displayAlertSuccess();
+    };
+
+    form.on('submit', submitedForm);
   }
 
-  popup();
-  displayAlertSuccessSubmit();
+  function displayAlertSuccess() {
+    var alert = $('.js-alert');
+    var toggleVisibleAlert = function(isVisible) {
+      alert.toggle(isVisible);
+    };
+
+    toggleVisibleAlert(true);
+
+    setTimeout(function() {
+      toggleVisibleAlert(false);
+    }, 3000);
+  }
+
+  pressButton();
 });
 
 
