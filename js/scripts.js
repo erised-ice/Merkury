24ae1046e7cce97054ea226e382a679adacebe5e
alexandra <le.onze.mai@gmail.com>
2019-01-28 20:02:15 +0300

    [form]changed js

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,29 @@ $(document).ready(function() {
   $('.js-slider').slick({
     dots: true
   });
+
+  function popup() {
+    var openPopupButtonCollection = $('.js-open-popup');
+    var closePopupCollection = $('.js-close-popup');
+    var popup = $('.js-popup');
+    var openPopup = function() {
+      popup.addClass('popup--show');
+    };
+    var closePopup = function() {
+      popup.removeClass('popup--show');
+    };
+
+    openPopupButtonCollection.on('click', openPopup);
+    closePopupCollection.on('click', closePopup);
+
+    $(document).on('keydown', function (event) {
+      if (event.keyCode == 27) {
+        closePopup()
+      }
+    });
+  }
+
+  popup();
 });
+
+
